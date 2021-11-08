532fac8e234a035004ac4e3615f967e7ef1372e4
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2021-11-08 22:35:32 +0300

    feat: add tasks solutions

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const fs = require('fs');
 
@@ -8,13 +7,3 @@ const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'ut
 readableStream.on('data', chunk => {
   console.log(chunk);
 });
-=======
-const path = require('path');
-const fs = require('fs');
-
-const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
-
-readableStream.on('data', chunk => {
-  console.log(chunk);
-});
->>>>>>> 00ade4f39f59a22987e4f1646ba9a088dfdf5493
